095788ce57a5c1bf55f2a4ff37b4f20fe4e61b3d
React-Projects <ashishkamaliya1@gmail.com>
2025-09-18 14:46:14 +0530

    Heading change

--- src/todo-app/MainHeading.jsx
+++ src/todo-app/MainHeading.jsx
@@ -1,7 +1,7 @@
 import { memo } from "react";
 
 function MainHeading() {
-    return <h1 className="my-3">Todo List</h1>;
+    return <h1 className="my-3">Todo Lists</h1>;
 }
 export default memo(MainHeading);
 
